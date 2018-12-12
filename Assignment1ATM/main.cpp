96f092ae8d64a88c9994ac1c049246da7f403334
Tyler <smitht9@vcu.edu>
2018-12-12 17:21:09 -0500

    Initial Commit

---
+++ Assignment1ATM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1ATM
+//
+//  Created by Tyler Smith on 12/12/18.
+//  Copyright © 2018 Tyler Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
